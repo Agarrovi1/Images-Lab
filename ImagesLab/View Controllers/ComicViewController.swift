89673ca162adec527f952b77c534f519ecf1a9d9
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-05 17:39:25 -0400

    adds func and outlets so stepper and buttons update image

--- ImagesLab/View Controllers/ComicViewController.swift
+++ ImagesLab/View Controllers/ComicViewController.swift
@@ -17,6 +17,12 @@ class ComicViewController: UIViewController {
     var currentComic = XkcdComic() {
         didSet {
             loadPicture()
+            comicTextField.placeholder = " \(currentComic.num): \(currentComic.safe_title)"
+            if currentComic.num == recentComic.num {
+                recentButton.isEnabled = false
+            } else {
+                recentButton.isEnabled = true
+            }
         }
     }
     var recentComic = XkcdComic() {
@@ -24,9 +30,39 @@ class ComicViewController: UIViewController {
             comicStepper.maximumValue = Double(recentComic.num)
             comicStepper.minimumValue = 1
             comicStepper.stepValue = 1
+            comicStepper.value = Double(recentComic.num)
         }
     }
     
+    @IBAction func recentButtonPressed(_ sender: UIButton) {
+        currentComic = recentComic
+        sender.isEnabled = false
+    }
+    
+    @IBAction func stepperStepped(_ sender: UIStepper) {
+        let newUrl = "https://xkcd.com/\(Int(sender.value))/info.0.json"
+        updateComic(with: newUrl)
+    }
+    @IBAction func randomButtonPressed(_ sender: UIButton) {
+        let randomNum = Int.random(in: 1...recentComic.num)
+        let newUrl = "https://xkcd.com/\(randomNum)/info.0.json"
+        updateComic(with: newUrl)
+    }
+    
+    
+    func updateComic(with url: String) {
+        XkcdComic.getComic(from: url) { (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let comic):
+                DispatchQueue.main.async {
+                    self.currentComic = comic
+                    self.loadPicture()
+                }
+            }
+        }
+    }
     func loadCurrentComic() {
         let recentUrl = "https://xkcd.com/info.0.json"
         XkcdComic.getComic(from: recentUrl) { (result) in
