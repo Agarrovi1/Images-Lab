91c7472abd9e6dcb28a13527b891d59f3dc7fecf
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-06 19:55:10 -0400

    adds users vc with a tableview

---
+++ ImagesLab/View Controllers/UsersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UsersViewController.swift
+//  ImagesLab
+//
+//  Created by Angela Garrovillas on 9/6/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+    @IBOutlet weak var usersTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
