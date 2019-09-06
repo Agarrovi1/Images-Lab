40d98664bda96bc19407069abda8a70b317c75a4
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-06 14:23:20 -0400

    adds vc for pokemon card

---
+++ ImagesLab/View Controllers/PokemonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonViewController.swift
+//  ImagesLab
+//
+//  Created by Angela Garrovillas on 9/6/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
