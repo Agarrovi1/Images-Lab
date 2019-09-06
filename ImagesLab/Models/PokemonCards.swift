f13e2b112ca940a659756205cd7f9128a2725da6
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-06 14:50:31 -0400

    adds PokemonCards struct

---
+++ ImagesLab/Models/PokemonCards.swift
@@ -0,0 +1,38 @@
+//
+//  PokemonCards.swift
+//  ImagesLab
+//
+//  Created by Angela Garrovillas on 9/6/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
+
+struct PokemonCards: Codable {
+    let cards: [Cards]
+    
+    static func getCards(completionHandler: @escaping (Result<[Cards],AppError>) -> () ) {
+        let url = "https://api.pokemontcg.io/v1/cards"
+        NetworkManager.shared.fetchData(urlString: url) { (result) in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let data):
+                do {
+                    let pokeCards = try JSONDecoder().decode(PokemonCards.self, from: data)
+                    completionHandler(.success(pokeCards.cards))
+                } catch {
+                    completionHandler(.failure(.noDataError))
+                }
+            }
+        }
+    }
+}
+
+struct Cards: Codable {
+    let name: String
+    let supertype: String
+    let imageUrl: String
+    let imageUrlHiRes: String
+    let set: String
+}
