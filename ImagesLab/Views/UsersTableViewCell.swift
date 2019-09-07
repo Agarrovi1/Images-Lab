df037ed88db40589f02338eabda02f47a15cbd67
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-06 20:01:32 -0400

    makes outlets for image and labels in custom user table view cell

---
+++ ImagesLab/Views/UsersTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  UsersTableViewCell.swift
+//  ImagesLab
+//
+//  Created by Angela Garrovillas on 9/6/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewCell: UITableViewCell {
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var cellPhoneLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
